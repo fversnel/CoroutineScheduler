8aaf35239cae1a5898fcaaf58eb5765c44e5b05b
Frank Versnel <f.versnel@gmail.com>
2015-11-17 10:39:26 +0100

    Added Update() to ICourtineScheduler interface.

--- CoroutineScheduler/CoroutineScheduler.cs
+++ CoroutineScheduler/CoroutineScheduler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace RamjetAnvil.Coroutine {
     public interface ICoroutineScheduler {
         IDisposable Run(IEnumerator<WaitCommand> fibre);
+        void Update(int currentFrame, float currentTime);
     }
 
     public class CoroutineScheduler : ICoroutineScheduler {
